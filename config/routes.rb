7d017c6e583e7268ad1a34f6c9d2107d21b7f48f
BidanGitau <bydangitau@gmail.com>
2023-01-04 12:00:11 +0300

    addicons

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "chatroom#index"
-<<<<<<< HEAD
   get "login" ,to:"sessions#new"
   
-=======
-  get "login" ,to: "sessions#new"
->>>>>>> main
 end
