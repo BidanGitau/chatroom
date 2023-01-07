512a682b09019db9284d545a15c1f76cbf933751
BidanGitau <bydangitau@gmail.com>
2023-01-07 09:19:19 +0300

    minor changes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
     validates :body, presence: true
-    belongs_to :user  
+    belongs_to :user 
+    scope :custom_display, -> { order(created_at: :desc).last(5)}
+    # Ex:- scope :active, -> {where(:active => true)} 
 end
